5b37ac8876f199d13224bcd927dee8fe9d54015f
Rodrigo Barreto <rodrigobrs123@gmail.com>
2025-10-13 11:12:13 -0300

    5) Loja com erro no Pagto e Login/Logout/Cadastro

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CartController;
 use App\Http\Controllers\Api\OrderController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 // Categorias
 Route::get('/categories', [CategoryController::class, 'index']);
@@ -16,17 +15,16 @@ Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/featured', [ProductController::class, 'featured']);
 Route::get('/products/{product}', [ProductController::class, 'show']);
 
-// Carrinho
-Route::group(['middleware' => ['web']], function () {
+// Carrinho - adicionar middleware 'web' para habilitar sessões
+Route::middleware('web')->group(function () {
     Route::get('/cart', [CartController::class, 'index']);
     Route::post('/cart', [CartController::class, 'store']);
     Route::put('/cart/{cartItem}', [CartController::class, 'update']);
     Route::delete('/cart/{cartItem}', [CartController::class, 'destroy']);
     Route::delete('/cart', [CartController::class, 'clear']);
-    
-    // Pedidos
-    Route::post('/orders', [OrderController::class, 'store']);
 });
 
+// Pedidos
+Route::post('/orders', [OrderController::class, 'store']);
 Route::get('/orders/{order}', [OrderController::class, 'show']);
 Route::post('/orders/payment-callback', [OrderController::class, 'paymentCallback']);
